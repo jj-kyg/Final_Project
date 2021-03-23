70a214c5da500109bbf031bc9d71eed56f35b294
Justin Johnson <bbohes@gmail.com>
2021-03-22 21:03:40 -0500

    database folder

---
+++ db/database.sql
@@ -0,0 +1,36 @@
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  isActive Boolean,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  description VARCHAR(255),
+  cost INTEGER NOT NULL,
+  featured Boolean NOT NULL,
+  onHand INTEGER NOT NULL,
+  keywords VARCHAR(255) UNIQUE NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  photos VARCHAR(255) UNIQUE NOT NULL
+)
+
+CREATE TABLE reviews (
+  id SERIAL PRIMARY KEY,
+  reviewId INTEGER NOT NULL,
+  name VARCHAR(255) UNIQUE NOT NULL,
+  rating INTEGER
+)
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  isActive Boolean,
+  isAdmin Boolean,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL,
+)
+
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  itemNumber INTEGER NOT NULL,
+  quantity INTEGER NOT NULL,
+  cost INTEGER NOT NULL,
+)
